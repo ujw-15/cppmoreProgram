0358f7b3715d597fc3a29331dde12d31ac74436e
uw15-boop <umsunggyu@gmail.com>
2026-03-16 21:40:20 +0900

    (2026,3,16) 단일 연결 리스트

---
+++ Program/Program.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class List
+{
+private:
+	struct Node
+	{
+		T data;
+		Node* next;
+	};
+
+	Node* head;
+	int size;
+
+public:
+	List()
+	{
+		head = nullptr;
+		size = 0;
+	}	
+
+	void push_front(T data)
+	{
+		Node* newNode = new Node;
+		newNode->data = data;
+
+		if (head == nullptr)
+		{
+			head = newNode;
+			newNode->next = nullptr;
+		}
+		else
+		{
+			newNode->next = head;
+			head = newNode;
+		}
+		size++;
+	}
+
+	void pop_front()
+	{
+		if (head == nullptr)
+		{
+			cout << "linked list is empty" << endl;
+		}
+		else
+		{
+			Node* deleteNode = head;
+
+			head = deleteNode->next;
+
+			delete deleteNode;
+
+			size--;
+		}
+	}
+};
+
+int main()
+{
+	List<int>list;
+
+	list.push_front(10);
+	list.push_front(5);
+
+	list.pop_front();
+	list.pop_front();
+	list.pop_front();
+	
+	return 0;
+}
+
