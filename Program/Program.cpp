3be806018c75c9d8447d72d02bb33a798f309b34
uw15-boop <umsunggyu@gmail.com>
2026-03-26 21:44:13 +0900

    (2026,3,26) 해시 테이블

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,10 +1,114 @@
 ﻿#include <iostream>
 
 using namespace std;
+template<typename T>
+class PriorrtyQueue
+{
+private:
+	int index;
+	int capacity;
+
+	T* container;
+public:
+	PriorrtyQueue()
+	{
+		index = 0;
+		capacity = 0;
+		container = nullptr;
+	}
+
+	void resize(int newSize)
+	{
+		
+		capacity = newSize;
+
+		T* temporary = new T[capacity];
+
+		for (int i = 0; i < capacity; i++)
+		{
+			temporary[i] = NULL;
+		}
+
+		for (int i = 0; i < index; i++)
+		{
+			temporary[i] = container[i];
+		}
+		delete[] container;
+
+		container = temporary;
+		
+	}
+
+	void push(T data)
+	{
+		if (capacity <= 0)
+		{
+			resize(1);
+		}
+		else if (index >= capacity)
+		{
+			resize(capacity * 2);
+		}
+
+		container[index++] = data;
+
+		int child = index - 1;
+		int parent = (child - 1) / 2;
+
+		while (child > 0)
+		{
+			if (container[parent] < container[child])
+			{
+				std::swap(container[parent], container[child]);
+			}
+
+			child = parent;
+
+			parent = (child - 1) / 2;
+		}
+	}
+
+	void pop()
+	{
+
+	}
+
+	~PriorrtyQueue()
+	{
+		if (container != nullptr)
+		{
+			delete[] container;
+		}
+	}
+
+	const T & top()
+	{
+		return container[0];
+	}
+
+	const int& size()
+	{
+		return size;
+	}
+
+	const bool& empty()
+	{
+		return index <= 0;
+	}
+};
 
 int main()
 {
+	PriorrtyQueue<int>priorrtyQueue;
 
+	priorrtyQueue.push(10);
+	priorrtyQueue.push(50);
+	priorrtyQueue.push(70);
+	priorrtyQueue.push(5);
+	priorrtyQueue.push(30);
+
+	cout << "Priority Queue is Size : " << priorrtyQueue.size() << endl;
 
 	return 0;
 }
+
